413e2f921b2259f563721aeb6340fab5dfbf0c4e
Tayo-bet <tayo4ayo@gmail.com>
2021-03-16 11:29:27 +0000

    profile.html

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def register():
         # New user 'Session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
 
@@ -60,7 +61,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Hello, {}".format(request.form.get("username")))
+                    flash("Hello, {}".format
+                      (request.form.get("username")))
+                    return redirect(url_for
+                      ("profile", username=session["user"]))
             else:
                 # wrong password match
                 flash("Wrong Username and/or Password")
@@ -72,6 +76,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # To get the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)    
 
 
 if __name__ == "__main__":
